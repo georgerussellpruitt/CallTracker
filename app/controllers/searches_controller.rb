72db5957bb3f2fe33f117cfc2eed27992ff445db
Russell Pruitt <pruitt.russell@gmail.com>
2012-06-12 13:49:10 -0400

    Search features completed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -37,10 +37,24 @@ class SearchesController < ApplicationController
 
   # POST /searches/rep
   def rep
+    @results = Call.where( :user_id => params[:uid] )
+
+    respond_to do |format|
+      format.html # type.html.erb
+      format.json { render json: @results }
+    end
   end
 
   # POST /searches/date
   def date
+    @srch_date_start = Date.civil(params[:search_date][:year].to_i, params[:search_date][:month].to_i, params[:search_date][:day].to_i)
+    @srch_date_end = Date.civil(params[:search_date][:year].to_i, params[:search_date][:month].to_i, params[:search_date][:day].to_i + 1)
+    @results = Call.where( :call_start => ( @srch_date_start..@srch_date_end ) )
+
+    respond_to do |format|
+      format.html # type.html.erb
+      format.json { render json: @results }
+    end
   end
 
 
